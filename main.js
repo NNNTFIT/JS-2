bd9f55164697718d28801353047b8be1b82e65b9
NNTFIT <nnt000fit@gmail.com>
2026-01-01 00:46:16 +0700

    fgyhjngj

--- main.js
+++ main.js
@@ -2,14 +2,14 @@
 const A = ["Bún bò", "Cơm tấm", "Gà rán", "Mì cay", "Phở", "Bún đậu", "Cơm chiên", "Bánh cuốn", "Bún thịt nướng", "Mì xào", "Cơm sườn", "Cháo lòng", "Bún mắm", "Cơm gà xối mỡ", "Bánh canh"];
 
 //hàm random các món ăn có trong danh sách
-function randomFood() {
+function getRandomFood() {
     const random = Math.floor(Math.random() * A.length); //lấy ngẫu nhiên 1 số trong phạm vi chiều dài của danh sách
     return A[random]; //trả về 1 món ăn ngẫu nhiên trong danh sách
 }
 
 //hàm in ra món ăn random
 function showFood() {
-    let food = randomFood(); // lấy 1 món ăn ngẫu nhiên có trong danh sách
+    let food = getRandomFood(); // lấy 1 món ăn ngẫu nhiên có trong danh sách
     document.getElementById("recommend").innerHTML = `Hôm nay bạn nên ăn: ${food}`; //in món ăn đó ra
 }
 showFood(); //in ra món ăn gợi ý sau khi vào trang web
