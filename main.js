4fa2177ffed73a266f0ffc6d8560fdaf61419448
NNTFIT <nnt000fit@gmail.com>
2025-12-31 23:58:52 +0700

    hehehehhe

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-const A = ["Bún bò", "Cơm tấm", "Gà rán", "Mì cay", "Phở"];
+const A = ["Bún bò", "Cơm tấm", "Gà rán", "Mì cay", "Phở", "Bún đậu", "Cơm chiên", "Bánh cuốn", "Bún thịt nướng", "Mì xào", "Cơm sườn", "Cháo lòng", "Bún mắm", "Cơm gà xối mỡ", "Bánh canh"];
 function randomFood() {
-  const random = Math.floor(Math.random() * A.length);
-  return A[random];
+    const random = Math.floor(Math.random() * A.length);
+    return A[random];
 }
 function showFood() {
-  const food = randomFood();
-  document.getElementById("recommend").innerHTML =
-    `Hôm nay bạn nên ăn: ${food}`;
+    const food = randomFood();
+    document.getElementById("recommend").innerHTML = `Hôm nay bạn nên ăn: ${food}`;
 }
 showFood();
